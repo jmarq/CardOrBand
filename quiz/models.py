e02f48d17a14732baffae4df19a8f163f4924d6a
Jimbo <marquardtjames@gmail.com>
2018-01-15 12:20:48 -0600

    cleared db, implemented base model

--- quiz/models.py
+++ quiz/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 
-class Card(models.Model):
+class Question(models.Model):
+    # base model for Card and Band models
     name = models.CharField(max_length=200, null=False, blank=False)
     correct = models.IntegerField(null=False, blank=False, default=0)
     incorrect = models.IntegerField(null=False, blank=False, default=0)
@@ -20,18 +21,9 @@ class Card(models.Model):
         self.save()
 
 
-class Band(models.Model):
-    name = models.CharField(max_length=200, null=False, blank=False)
-    correct = models.IntegerField(null=False, blank=False, default=0)
-    incorrect = models.IntegerField(null=False, blank=False, default=0)
+class Card(Question):
+    pass
 
-    def __str__(self):
-        return self.name
 
-    def add_correct(self):
-        self.correct += 1
-        self.save()
-
-    def add_incorrect(self):
-        self.incorrect += 1
-        self.save()
+class Band(Question):
+    pass
